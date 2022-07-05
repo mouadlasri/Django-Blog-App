77e93eab61fbf87917115599b807cd145d08e78e
Mouad <lasri.mouad98@gmail.com>
2022-07-05 18:18:33 +0900

    Login and Logout

--- config/settings.py
+++ config/settings.py
@@ -127,3 +127,9 @@ STATICFILES_DIRS = [str(BASE_DIR.joinpath('static'))]
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+# new
+LOGIN_REDIRECT_URL = 'home' 
+
+LOGOUT_REDIRECT_URL = 'home'
